b0d92f52cfd2a881f1e88e2eb666488958bf162d
Dallan Freemantle <dallan@alphero.com>
2019-06-18 13:10:26 +1200

    First commit

--- src/index.js
+++ src/index.js
@@ -1,31 +1,9 @@
-import _ from 'lodash';
-import printMe from './print.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import './styles.css';
+const title = 'My Minimal React Webpack Babel Setup';
 
-function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-  return element;
-}
-
-let element = component();
-document.body.appendChild(element);
-
-// Hot module replacement
-if (module.hot) {
-  module.hot.accept('./print.js', function() {
-    console.log('Accepting the updated printMe module!');
-    document.body.removeChild(element);
-    element = component(); // Re-render the "component" to update the click handler
-    document.body.appendChild(element);
-  })
-}
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,37 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: {
     app: './src/index.js'
   },
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist',
-    hot: true
+    contentBase: './dist'
   },
   module: {
     rules: [
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
       }
     ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Output Management'
-    }),
-    new webpack.HotModuleReplacementPlugin()
+      template: './src/index.html'
+    })
   ],
   output: {
     filename: '[name].bundle.js',

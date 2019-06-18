410addc6282b0be5f4663b779720f9d11d3a7a04
Dallan Freemantle <dallan@alphero.com>
2019-06-18 12:41:46 +1200

    Add HMR

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 import _ from 'lodash';
+import printMe from './print.js';
+
+import './styles.css';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
 
-document.body.appendChild(component());
+let element = component();
+document.body.appendChild(element);
+
+// Hot module replacement
+if (module.hot) {
+  module.hot.accept('./print.js', function() {
+    console.log('Accepting the updated printMe module!');
+    document.body.removeChild(element);
+    element = component(); // Re-render the "component" to update the click handler
+    document.body.appendChild(element);
+  })
+}
---
+++ src/print.js
@@ -0,0 +1,3 @@
+export default function printMe() {
+  console.log('I get calleddd from print.js!');
+}
